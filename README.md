7696d70249cf5a8f6b2767f81e7f529410217634
Anthony Michelizzi <cynical89@gmail.com>
2016-10-17 14:48:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# FTG Store Desktop
+A desktop app to use the store view on Food-to-Go!
+
+## Prerequisites
+* [Node.js](https://nodejs.org/en/) (Version 5 and up recommended)
+* [food-to-go](https://github.com/cynical89/food-to-go) (you need the main project to run the app since it is also the api server)
+* [food-to-go Prerequisites](https://github.com/cynical89/food-to-go/blob/master/README.md#prerequisites) (you need these to run the main project)
+
+### Installation
+
+* Clone down the repository.
+```
+git clone https://github.com/cynical89/ftg-store-desktop.git
+```
+
+* Install packages (from inside the food-to-go folder).
+```
+npm install
+```
+
+* Start it up.
+```
+npm start
+```
+* Enjoy!
---
+++ js/login.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+	$("#submitBtn").on("click", function(e) {
+		e.preventDefault();
+		var username = $("#user").val();
+		var password = $("#pass").val();
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "http://localhost:5000/api/login/local",
+			data: {username: username, password: password},
+		}).done(function(result) {
+			if (result.error === true) {
+				alert(result.message);
+				return console.error(result.message);
+			}
+				localStorage.setItem("user", result);
+				window.location = "/pages/account.html";
+		}).fail(function(err) {
+			// do something with the failure, like laugh at the user
+			window.alert("hahahahaha! NO!");
+			console.error(err);
+		});
+	});
+});
